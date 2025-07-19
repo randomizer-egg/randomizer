44116dc63ced143d3d3059b58de71b5813e3c430
randomizer-egg <louisenzolegend@gmail.com>
2025-07-19 08:41:18 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomizer-egg/randomizer1src/refs/heads/main/egg%20randomizer%20source"))()
-loadstring(game:HttpGet("https://pastefy.app/Oa9BqsGb/raw"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/37vuWe5k2V/0", true))()

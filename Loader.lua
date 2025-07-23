94a3ec297f11aff89831aca3ce28cbd7de0e1640
randomizer-egg <louisenzolegend@gmail.com>
2025-07-23 11:27:42 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomizer-egg/randomizer1src/refs/heads/main/egg%20randomizer%20source"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/eHS3yjYGdzA4EEEz/raw", true))()

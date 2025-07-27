871e12d6945b88bcde359dd8808f3939b48525c4
randomizer-egg <louisenzolegend@gmail.com>
2025-07-27 20:44:57 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomizer-egg/randomizer1src/refs/heads/main/egg%20randomizer%20source"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/OhWiClDKLe2tdL4C/raw", true))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/pXWG2p80R78JsuDC/raw", true))()
